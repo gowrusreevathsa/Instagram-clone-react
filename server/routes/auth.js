b3cfa7cf0a081fe5ecd1909d69b4e8cdd32eceff
Sreevathsa <sreevathsagowru@gmail.com>
2020-09-12 20:10:29 +0530

    Sign In

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const User = mongoose.model("User");
+const bcrypt = require("bcryptjs");
 
 router.get("/", (req, res) => {
   res.send("Hello");
@@ -23,26 +24,56 @@ router.post("/signup", (req, res) => {
           error: "An account is already associated with that email",
         });
       }
-      const user = new User({
-        name,
-        email,
-        password,
-      });
 
-      user
-        .save()
-        .then((user) => {
-          res.json({
-            message: "User saved successfully",
-          });
-        })
-        .catch((err) => {
-          console.log(err);
+      bcrypt.hash(password, 12).then((hashedPassword) => {
+        const user = new User({
+          name,
+          email,
+          password: hashedPassword,
         });
+
+        user
+          .save()
+          .then((user) => {
+            res.json({
+              message: "User saved successfully",
+            });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      });
     })
     .catch((err) => {
       console.log(err);
     });
+});
+
+router.post("/signin", (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(422).json({
+      error: "Please provide both E-mail and Password",
+    });
+  }
+  User.findOne({ email: email }).then((savedUser) => {
+    if (!savedUser) {
+      return res.status(422).json({ error: "Invalid E-mail or Password" });
+    }
+    bcrypt
+      .compare(password, savedUser.password)
+      .then((match) => {
+        if (match) {
+          res.json({ message: "Sign-in is successful" });
+        } else {
+          return res.status(422).json({ error: "Invalid E-mail or password" });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 });
 
 module.exports = router;
