60f8ba126209c6c732bdfcdb4b7f06a0df1e244d
Sreevathsa <sreevathsagowru@gmail.com>
2020-09-12 18:23:32 +0530

    First commit

---
+++ server/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const PORT = 5000;
+const { MONGOURI } = require("./keys");
+
+require("./models/user");
+
+app.use(express.json());
+app.use(require("./routes/auth"));
+
+mongoose.connect(MONGOURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.on("connected", () => {
+  console.log("Mongo connected");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("Error while connecting to mongo: ", err);
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running on the port: ", PORT);
+});
---
+++ server/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MONGOURI:
+    "mongodb+srv://sreevathsa:v6Vs3UPKkCgHSXYs@cluster0.hfmhj.mongodb.net/default?retryWrites=true&w=majority",
+};
