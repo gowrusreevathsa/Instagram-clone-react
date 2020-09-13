b7dd5d8556c0d2ec53579e590897017d2af58ed5
Sreevathsa <sreevathsagowru@gmail.com>
2020-09-13 23:32:36 +0530

    Sign Up

--- client/src/components/screens/SignUp.js
+++ client/src/components/screens/SignUp.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 function SignUp() {
-  return <></>;
+  return (
+    <>
+      <div className="loginCard">
+        <div className="card auth-card input-field">
+          <h2>Instagram</h2>
+          <input type="text" placeholder="Name" />
+          <input type="text" placeholder="E-mail" />
+          <input type="password" placeholder="Password" />
+
+          <button className="btn waves-effect waves-light blue darken-2">
+            SignUp
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default SignUp;
