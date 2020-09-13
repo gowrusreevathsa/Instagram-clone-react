12cb416234daf11c78b3edd365b373094b1006e1
Sreevathsa <sreevathsagowru@gmail.com>
2020-09-13 23:32:22 +0530

    Sign in

--- client/src/components/screens/Login.js
+++ client/src/components/screens/Login.js
@@ -3,7 +3,17 @@ import React from "react";
 function Login() {
   return (
     <>
-      <h1>Login</h1>
+      <div className="loginCard">
+        <div className="card auth-card input-field">
+          <h2>Instagram</h2>
+          <input type="text" placeholder="E-mail" />
+          <input type="password" placeholder="Password" />
+
+          <button className="btn waves-effect waves-light blue darken-2">
+            Login
+          </button>
+        </div>
+      </div>
     </>
   );
 }
--- client/src/components/screens/SignUp.js
+++ client/src/components/screens/SignUp.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 function SignUp() {
-  return (
-    <>
-      <h1>Sign Up</h1>
-    </>
-  );
+  return <></>;
 }
 
 export default SignUp;
