599cf41d6cc94cc534f0a95f7b60c43429e2a8ea
Sreevathsa <sreevathsagowru@gmail.com>
2020-09-13 20:09:28 +0530

    Basic Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./components/screens/Home";
+import SignUp from "./components/screens/SignUp";
+import Login from "./components/screens/Login";
+import Profile from "./components/screens/Profile";
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Router>
+        <Navbar />
+
+        <Route exact path="/" component={Home} />
+        <Route path="/signup" component={SignUp} />
+        <Route path="/login" component={Login} />
+        <Route path="/profile" component={Profile} />
+      </Router>
     </>
   );
 }
