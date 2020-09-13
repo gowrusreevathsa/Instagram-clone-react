55b7e3bc1393e2412eed5182b5d3b7c0d8d90129
Sreevathsa <sreevathsagowru@gmail.com>
2020-09-14 00:55:12 +0530

    Home page

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -3,7 +3,20 @@ import React from "react";
 function Home() {
   return (
     <>
-      <h1>Home</h1>
+      <div className="home">
+        <div className="card home-card">
+          <h5>Sreevathsa Gowru</h5>
+          <div className="card-image">
+            <img src="https://images.unsplash.com/photo-1550056486-8ded219fb769?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80" />
+          </div>
+          <div className="card-content">
+            <i class="material-icons">favorite_border</i>
+            <h6>Title</h6>
+            <p>This is a post</p>
+            <input type="text" placeholder="Add a comment" />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
