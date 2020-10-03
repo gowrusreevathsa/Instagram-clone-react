c9f82b8c2ac6a92b68b148b95a8fc12e9251cf46
Sreevathsa <sreevathsagowru@gmail.com>
2020-10-03 17:23:36 +0530

    Update

--- server/keys.js
+++ server/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
   MONGOURI:
-    /*  MONGOURI HIDDEN FOR SECURITY REASONS*/,
+    "mongodb+srv://sreevathsa:v6Vs3UPKkCgHSXYs@cluster0.hfmhj.mongodb.net/default?retryWrites=true&w=majority",
   JWT_SECRET:
-    /*  JWT_SECRET HIDDEN FOR SECURITY REASONS*/,
+    "EAB6820A23BC43BEE23BAF9C4B8107EEEA2381D2CB0BB4A55F787EB78E235992",
 };
